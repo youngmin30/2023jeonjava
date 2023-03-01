de28ca2f93340d066214804d1169612e021b83a1
youngmin30 <jjymjj@naver.com>
2023-03-02 00:55:43 +0900

    23020302 github test 이자다 chap02 변수

---
+++ src/chap02/src/sec01/exam01_variable/VariableScopeExample.java
@@ -0,0 +1,28 @@
+﻿package chap02.src.sec01.exam01_variable;
+
+public class VariableScopeExample {
+	public static void main(String[] args) {
+		
+		int var1; // 정수형 변수
+		
+		if(true) { // true이면 아래 실행
+			int var2;
+			
+			var1 = 10; // 변수
+			var2 = 20; // 변수
+		}
+		
+		var1 = 10;
+		//var2 = 20;
+		
+		for(int i=0; i<1; i++) {
+			int var3;
+			
+			var1 = 10;
+			//var3 = 30;
+		}
+		
+		var1 = 10;
+		//var3 = 30;
+	}
+}
---
+++ src/chap02/src/sec02/exam02_char/CharExample.java
@@ -0,0 +1,23 @@
+﻿package chap02.src.sec02.exam02_char;
+
+public class CharExample {
+	public static void main(String[] args) {
+		char c1 = 'A';          	//문자를 직접 저장
+		char c2 = 65;          	//십진수로 저장
+		char c3 = '\u0041';    //16진수로 저장
+		
+		char c4 = '가';         	//문자를 직접 저장
+		char c5 = 44032;      	//십진수로 저장
+		char c6 = '\uac00';    //16진수로 저장
+		
+		int uniCode = c1;		//유니코드 얻기
+		
+		System.out.println(c1); // A
+		System.out.println(c2); // A
+		System.out.println(c3); // A
+		System.out.println(c4); // 가
+		System.out.println(c5); // 가
+		System.out.println(c6); // 가
+		System.out.println(uniCode); // 65
+	} 
+}

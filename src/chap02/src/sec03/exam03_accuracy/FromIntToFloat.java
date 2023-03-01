83d9beafeed2606425b5308d8a0b421a119aaf55
youngmin30 <jjymjj@naver.com>
2023-03-02 02:37:49 +0900

    23020302 github test 이자다 chap02 변수

---
+++ src/chap02/src/sec03/exam03_accuracy/FromIntToFloat.java
@@ -0,0 +1,17 @@
+﻿package chap02.src.sec03.exam03_accuracy;
+public class FromIntToFloat {
+	public static void main(String[] args) {
+		
+		int num1 = 123456780;
+		int num2 = 123456780;
+		
+		float num3 = num2;
+		// System.out.println(num2); // 123456780
+		num2 = (int) num3;
+		// System.out.println(num3); // 1.23456784E8
+		
+		int result = num1 - num2; // 같은 타입끼리 연산함
+		System.out.println(result);
+	}
+}
+

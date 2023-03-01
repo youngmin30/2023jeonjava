a5c972576a0c792b8a2e948073fbd82aa7504c10
youngmin30 <jjymjj@naver.com>
2023-03-02 00:48:51 +0900

    23020302 github test java Hello08.java test

--- src/chap01/Hello08.java
+++ src/chap01/Hello08.java
@@ -1,8 +1,7 @@
 package chap01;
 
-public class Hello07 {
+public class Hello08 {
     public static void main(String[] args) {
         System.out.println("Hello, welcome to the java world!");
     }
 }
-got ;p
